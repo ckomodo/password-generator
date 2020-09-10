54b7f791f1905322fb16e6ec0ef912ad208b5031
ckomodo <chrissakwa@gmail.com>
2020-09-10 15:20:48 -0700

    finished pseudo code on read me

--- script.js
+++ script.js
@@ -46,9 +46,24 @@ function generatePassword(){
 // TODO: create a "confirm" , ask user if they want lower case chars
 // TODO: create a "confirm" , ask user if they want upper case chars
 // TODO: create a "confirm" , ask user if they want special chars
-// TODO: create a "confirm" , ask user if they want numbers chars
+// TODO: create a "confirm" , ask user if they want numbers chars 
+// TODO: add validations to make sure at least one type of char is selected, alert user if no type has been selected
+// TODO: create arrays for all needed characters; upper case and lower case
+// TODO: create a variable that holds the users choice of password
+// TODO: create a conditional situation that holds the user choices(use if statements to check user choice)
+// TODO: create a new variable that holds all the chosen characters
+// TODO: validate that password length entered is at least 8 chars and less than 128 chars
+// TODO: create set number of characters randomly for the user. 
+//TODO: use math.floor(math.random()) to get a random character--create a for loop to loop the length of chosen characters(for)
+// TODO: create a random password at the end
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// var number = [0-9]
+for(vari = o; i < userCharLength; i++);
