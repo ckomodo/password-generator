7bd943e415dabd32334c9e02cf958291c353f00a
ckomodo <chrissakwa@gmail.com>
2020-09-10 09:36:55 -0700

    function generatePassword added

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+
+
+// button.addEventListener(click, function()){
+
+// prompt("how many characters do you want in your password?");
+
+// }
+// function generatePassword
+
+  // (characterAmount, includeUppercase, includeNumbers, includeSymbols)
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +30,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword(){
+
+  var userCharNum =   prompt("how many characters do you want in your password?");
+  console.log(userCharNum);
+
+  var specialChar = prompt("do you want any special characters?");
+  console.log(specialChar);
+
+
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
